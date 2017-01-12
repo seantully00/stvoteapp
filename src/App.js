56c6793b00dff1f11df37955548b921f42447ec9
Sean Tully <seantully00@hotmail.com>
2017-01-11 21:11:18 -0500

    onclicks

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.jpg';
 import './App.css';
 
-function login() {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Votejere</h2>
-        </div>
-        <p className="App-intro">
-          Votejere!
-        </p>
-        <br></br>
-        <button onClick={findpoll}>Find a Poll</button>
-      </div>
-    );
-  }
-}
-
 class App extends Component {
+
+  function login() {
+    render() {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Votejere</h2>
+          </div>
+          <p className="App-intro">
+            Votejere!
+          </p>
+          <br></br>
+          <button onClick={findpoll}>Find a Poll</button>
+        </div>
+      );
+    }
+  },
   render() {
     return (
       <div className="App">
